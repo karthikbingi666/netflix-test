5ec7122822ff1cf83cfd89cf9c984798e8fb5c24
刘凤玉 <liufengyu@le.com>
2016-11-23 16:46:55 +0800

    init

---
+++ src/main/java/com/lecloud/soa/EurekaServer.java
@@ -0,0 +1,11 @@
+package com.lecloud.soa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServer {
+	public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
